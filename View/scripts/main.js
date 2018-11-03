4de76ae622098934fec684bf83a9778f3a9b9376
winxeater <matheus.a.aguiar@hotmail.com>
2018-11-03 04:49:28 -0300

    alteracoes no negocio todo

--- View/scripts/main.js
+++ View/scripts/main.js
@@ -1,7 +1,43 @@
 
 $(document).ready(function(){
-    $("h2").click(function(){
-        $("p").toggle();
+    $("#addnew").on('click',function(){
+        $("#tabela").modal('show');
     });
 });
 
+
+
+function salvarDados(key){
+    var usuario=$("#usuario");
+    var senha=$("#senha");
+    var email=$("#email");
+    var cpf=$("#cpf");
+
+    if(isNotEmpty(usuario) && isNotEmpty(senha) && isNotEmpty(email) && isNotEmpty(cpf)){
+        $.ajax({
+            url: 'ajax.php',
+            method: 'POST',
+            dataType: 'text',
+            data: {
+                key: ID,
+                usuario: usuario.val(),
+                senha: senha.val(),
+                email: email.val(),
+                cpf: cpf.val(),
+            },
+            success: function (response) {
+                alert(response);
+            }
+        });
+    }
+}
+
+function isNotEmpty(caller){
+    if (caller.val()==''){
+        caller.css('border','1px solid red');
+        return false;
+    } else
+        caller.css('border','');
+
+        return true;
+}
